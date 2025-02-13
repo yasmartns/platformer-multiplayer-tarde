7942149208b5b33cd36bca860cb8db7f5efd7e59
yasmartns <yasmingomes2977@gmail.com>
2025-02-13 16:51:08 -0300

    inicial

---
+++ actors/character_body_2d.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction == -1:
+		animacao.flip_h = true
+	elif direction == 1:
+		animacao.flip_h = false
+		
+	if velocity.y < 0:
+		animacao.play("jump")
+	elif velocity.y > 0:
+		animacao.play("fall")
+	elif velocity.x != 0:
+		animacao.play("run")
+	else:
+		animacao.play("idle")
+
+		
+		
+			
+
+	move_and_slide()
